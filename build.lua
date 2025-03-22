bbbaa812a1d6f8e2b66c9c7acca54d8170025275
Mingyu Xia <myhsia@outlook.com>
2025-03-22 21:36:15 +0800

    Version 1.1.1 released     - Fixed the lost of graphics files when installing by tlmgr (x2).     - Updated the l3build workflow.     - Reconstructed hdu-graphicx.dtx.

--- build.lua
+++ build.lua
@@ -21,22 +21,18 @@ uploadconfig = {
   uploader     = "Mingyu Xia",
   email        = "myhsia@outlook.com",
   summary      = [[
-    LaTeX class for bachelor and MPhil theses in
-    Hangzhou Dianzi University
+    LaTeX class for bachelor and MPhil theses in Hangzhou Dianzi University
   ]],
   description  = [[
-    This package provides a LaTeX template for graduation theses in
-    Hangzhou Dianzi University.
-    It supports the formatting of bachelor and MPhil degree theses.
+    This package provides a LaTeX template for graduation theses in Hangzhou Dianzi University. It supports the formatting of bachelor and MPhil degree theses.
   ]],
   license      = "lppl1.3c",  
   ctanPath     = "/macros/latex/contrib/" .. module,
   announcement = [[
-    Version 1.1.0 released
-    - Fixed the lost of graphics files when installing by tlmgr.
+    Version 1.1.1 released
+    - Fixed the lost of graphics files when installing by tlmgr (x2).
     - Updated the l3build workflow.
-    - Finished the hdu-exam module.
-    - Fixed the support of ApLaTeX compiler.
+    - Reconstructed hdu-graphicx.dtx.
   ]],
   home         = "https://github.com/myhsia/" .. module,
   bugtracker   = "https://github.com/myhsia/" .. module .. "/issues",
